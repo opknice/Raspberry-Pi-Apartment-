219904ccfadf399ee193b54308496f8bb4af7013
opknice <thanakrit_kas@hotmail.com>
2019-07-30 14:47:11 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,14 +2,15 @@
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="root",
-  passwd="cpe54146",
-  database="log_data"
+  user="myusername",
+  passwd="mypassword",
+  database="iot_device"
 )
 mycursor = mydb.cursor()
 
-sql = 'INSERT INTO log_data(ROOM, STATUS, DATE) VALUES ('5', 'OFF', '2019-07-30')'
-mycursor.execute(sql)
+sql = 'INSERT INTO iot(iot_name, iot_value1, iot_value2) VALUES (%s, %s, %s)'
+val = ('TEMP', '20', '25')
+mycursor.execute(sql, val)
 
 mydb.commit()
 print(mycursor.rowcount, "record inserted.")
